bbdf9af2c17ca1cd90634ab9b3200ff730697d39
Ryuji Yasukochi <ryuji.yasu@gmail.com>
2021-06-10 13:55:36 +0900

    Diaryの追加

---
+++ database/migrations/2021_05_28_062045_create_diaries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->text('status');
+            $table->datetime('datetime');
+            $table->text('momentum');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diaries');
+    }
+}
