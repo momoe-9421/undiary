82d391bcef5ade5ff77c701e3ace9458363147f0
Tsuruoka Momoe <momon_9421@yahoo.co.jp>
2021-06-10 14:20:37 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/chart', 'HomeController@chart')->name('chart');
+Route::get('/about', 'HomeController@about')->name('about');
 
